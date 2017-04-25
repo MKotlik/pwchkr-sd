d29ae113554f5a492d4faa7cb5506f58e7a0d8cb
MKotlik <mkotlik@stuy.edu>
2017-04-25 19:33:48 -0400

    added debug and testing features

--- pswdcheck.py
+++ pswdcheck.py
@@ -3,6 +3,10 @@
 # Misha Kotlik
 
 
+# Global debug setting
+DEBUG = False
+
+
 # boolean verify_pass(password)
 def verify_pass(password):
     """Password verification function
@@ -13,7 +17,21 @@ def verify_pass(password):
     lower_chars = [c for c in password if c.islower()]
     upper_chars = [c for c in password if c.isupper()]
     numbers = [c for c in password if c.isdigit()]
+    if DEBUG:
+        print "Lower: " + str(lower_chars)
+        print "Upper: " + str(upper_chars)
+        print "Numbers: " + str(numbers)
     if lower_chars and upper_chars and numbers:
         return True
     else:
         return False
+
+
+if __name__ == "__main__":
+    DEBUG = False
+    print "Verifying password 'MyMilkshake3':"
+    print verify_pass("MyMilkshake3")
+    print "Verifying password 'bringstheboys':"
+    print verify_pass("bringstheboys")
+    print "Verifying password '3OH3':"
+    print verify_pass("3OH3")
